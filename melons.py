bf94e47b7d6565210354e880d014375dc9d07cd1
Addison McCabe <103456010+AddyMcCabe@users.noreply.github.com>
2022-06-20 13:42:34 -0700

    updated

--- melons.py
+++ melons.py
@@ -1,7 +1,9 @@
 """This file should have our melon-type classes in it."""
+class MelonOrder:
+    def get_base_price(self):
+        return 5.0
 
-
-class WatermelonOrder:
+class WatermelonOrder(MelonOrder):
     species = "Watermelon"
     color = "green"
     imported = False
@@ -11,13 +13,13 @@ class WatermelonOrder:
     def get_price(self, qty):
         """Calculate price, given a number of melons ordered."""
 
-        total = 5.0 * qty
+        total = self.get_base_price() * qty
         if qty >= 3:
             total = total * 0.75
         return total
 
 
-class CantaloupeOrder:
+class CantaloupeOrder(MelonOrder):
     species = "Cantaloupe"
     color = "tan"
     imported = "False"
@@ -27,13 +29,13 @@ class CantaloupeOrder:
     def get_price(self, qty):
         """Calculate price, given a number of melons ordered."""
 
-        total = 5.0 * qty
+        total = self.get_base_price() * qty
         if qty > 5:
             total / 2
         return total
 
 
-class CasabaOrder:
+class CasabaOrder(MelonOrder):
     species = "Casaba"
     color = "green"
     imported = "True"
@@ -41,10 +43,10 @@ class CasabaOrder:
     seasons = ["Spring", "Summer", "Fall", "Winter"]
 
     def get_price(self, qty):
-        total = (6.0 * 1.5) * qty
+        total = ((self.get_base_price() + 1) * 1.5) * qty
         return total
 
-class SharlynOrder:
+class SharlynOrder(MelonOrder):
     species = "Sharlyn"
     color = "tan"
     imported = "True"
@@ -52,11 +54,11 @@ class SharlynOrder:
     seasons = "Summer"
 
     def get_price(self, qty):
-        total = 5.0 * qty
+        total = self.get_base_price() * qty
         return total
 
 
-class SantaClausOder:
+class SantaClausOder(MelonOrder):
     species = "Santa Claus"
     color = "green"
     imported = "True"
